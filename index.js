9d3aa35d644548572b970c0be35b847936bf9d90
emmanuelist <emmanuel.paul75@yahoo.com>
2024-10-17 00:42:49 +0100

    Implement route for creating Google Calendar events

--- index.js
+++ index.js
@@ -32,6 +32,59 @@ app.get("/auth/redirect", async (req, res) => {
 	res.send('Authentication successful! Please return to the console.');
 });
 
+app.get('/create-event', async (req, res) => {
+    const { startDateTime, endDateTime, timeZone } = req.query;
+
+    if (!startDateTime || !endDateTime || !timeZone) {
+        return res.status(400).send({
+            status: 400,
+            message: 'Missing required query parameters: startDateTime, endDateTime, timeZone'
+        });
+    }
+
+    const event = {
+        summary: 'Tech Talk with Emmanuel',
+        location: 'Google Meet',
+        description: "Demo event for Emmanuel's Blog Post.",
+        start: {
+            dateTime: startDateTime,
+            timeZone: timeZone
+        },
+        end: {
+            dateTime: endDateTime,
+            timeZone: timeZone
+        },
+        colorId: 1,
+        conferenceData: {
+            createRequest: {
+                requestId: uuid(),
+            }
+        },
+        attendees: [
+            { email: 'tweetsonly7@gmail.com' },
+        ]
+    };
+
+    try {
+        const result = await calendar.events.insert({
+            calendarId: 'primary',
+            auth: oauth2Client,
+            conferenceDataVersion: 1,
+            sendUpdates: 'all',
+            resource: event
+        });
+
+        res.send({
+            status: 200,
+            message: 'Event created',
+            link: result.data.hangoutLink
+        });
+    } catch (err) {
+		console.log(err);
+        res.status(500).send(err);
+    }
+});
+
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
 });
