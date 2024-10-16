81935cd901c82cbc40322da70c261fc04d92fce4
emmanuelist <emmanuel.paul75@yahoo.com>
2024-10-16 01:41:38 +0100

    Add environment variables configuration

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+
+const PORT = process.env.PORT || 8000;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URL = process.env.REDIRECT_URL;
+const API_KEY = process.env.API_KEY;
