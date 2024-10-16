aaf01ba994354f937c28334fb80dcfd319d2bdfa
emmanuelist <emmanuel.paul75@yahoo.com>
2024-10-17 00:15:21 +0100

    Add route for Google OAuth 2.0 authentication

--- index.js
+++ index.js
@@ -6,16 +6,26 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
+const scopes = ['https://www.googleapis.com/auth/calendar'];
+
 const { google } = require('googleapis');
 
 const oauth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URL
+	process.env.CLIENT_ID,
+	process.env.CLIENT_SECRET,
+	process.env.REDIRECT_URL
 );
 
 const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
 
+app.get('/auth', (req, res) => {
+	const url = oauth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: scopes
+	});
+	res.redirect(url);
+});
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 });
