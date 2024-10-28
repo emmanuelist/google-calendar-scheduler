9950278f715685afb8a84b775cdd22d894c712cf
emmanuelist <emmanuel.paul75@yahoo.com>
2024-10-28 07:05:04 +0100

    Initialize Express server and configure environment variables

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { v4: uuid } = require('uuid');
+const cors = require('cors');
+const fs = require('fs').promises;
+const path = require('path');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
